bd9935b80a06644fa0b0df450ab0693ac39b9a0e
10sa <zenkins26@gmail.com>
2018-03-14 10:37:39 +0900

    Initiailize project, Implement basic memory model.

---
+++ UMemory/Models/Memory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace UMemory.Models
+{
+	/// <summary>
+	/// Implement Unmanaged memory area.
+	/// </summary>
+	public class Memory : IDisposable
+	{
+		private Memory() { }
+
+		public int Size { get; private set; }
+
+		public IntPtr Address { get; private set; }
+		public byte[] Bytes { get; set; }
+
+		internal static Memory Allocation(int size)
+		{
+			IntPtr memoryObject = Marshal.AllocHGlobal(size + Marshal.SizeOf(typeof(Memory)));
+			
+			return (Memory)Marshal.PtrToStructure(memoryObject, typeof(Memory));
+		}
+
+		/// <summary>
+		/// Dispose unmanaged memory area.
+		/// </summary>
+		public void Delete()
+		{
+			Marshal.FreeHGlobal(Address);
+			Address = IntPtr.Zero;
+		}
+
+		public bool Deleted { get
+			{
+				return Address == IntPtr.Zero;
+			}
+		}
+
+		public void Dispose()
+		{
+			Delete();
+		}
+	}
+}
