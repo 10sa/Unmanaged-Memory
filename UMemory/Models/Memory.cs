3276c916d5947a865525441fcc866615c9a5aa68
10sa <zenkins26@gmail.com>
2018-03-14 12:40:32 +0900

    Update memory methods.

--- UMemory/Models/Memory.cs
+++ UMemory/Models/Memory.cs
@@ -10,40 +10,103 @@ namespace UMemory.Models
 	/// <summary>
 	/// Implement Unmanaged memory area.
 	/// </summary>
-	public class Memory : IDisposable
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public class Memory : IDisposable, ICloneable
 	{
 		private Memory() { }
 
+		/// <summary>
+		/// Return allocated memory size.
+		/// </summary>
 		public int Size { get; private set; }
 
+		/// <summary>
+		/// Return allocated memory area address. This address isn't Memory insteance address.
+		/// </summary>
 		public IntPtr Address { get; private set; }
-		public byte[] Bytes { get; set; }
+
+		/// <summary>
+		/// Return this object address. This address isn't request memory address.
+		/// </summary>
+		public IntPtr ObjectAddress { get; private set; }
 
 		internal static Memory Allocation(int size)
 		{
 			IntPtr memoryObject = Marshal.AllocHGlobal(size + Marshal.SizeOf(typeof(Memory)));
-			
-			return (Memory)Marshal.PtrToStructure(memoryObject, typeof(Memory));
+			Memory memory = (Memory)Marshal.PtrToStructure(memoryObject, typeof(Memory));
+			memory.Address = IntPtr.Add(memoryObject, Marshal.SizeOf(typeof(Memory)));
+			memory.ObjectAddress = memoryObject;
+			memory.Size = size;
+
+			return memory;
 		}
 
 		/// <summary>
-		/// Dispose unmanaged memory area.
+		/// Cast allocated memory area.
 		/// </summary>
-		public void Delete()
+		/// <param name="dstType">Casting type.</param>
+		/// <returns>Castingable object.</returns>
+		public object Cast(Type dstType)
 		{
-			Marshal.FreeHGlobal(Address);
-			Address = IntPtr.Zero;
+			if (Marshal.SizeOf(dstType) != Size)
+				return Marshal.PtrToStructure(Address, dstType);
+			else
+				throw new InvalidCastException("Memory Size mismatched!");
 		}
 
-		public bool Deleted { get
+		/// <summary>
+		/// Cast allocated memory area.
+		/// </summary>
+		/// <param name="dstType">Casting type.</param>
+		/// <param name="ignoreOversize">If true, ignore oversize.</param>
+		/// <returns></returns>
+		public object Cast(Type dstType, bool ignoreOversize)
+		{
+			try
 			{
-				return Address == IntPtr.Zero;
+				return Cast(dstType);
 			}
+			catch (InvalidCastException)
+			{
+				if (Marshal.SizeOf(dstType) <= Size && ignoreOversize)
+					return Marshal.PtrToStructure(Address, dstType);
+				else
+					throw;
+			}
+		}
+
+		/// <summary>
+		/// If target object address equal, true. otherwise false.
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public override bool Equals(object obj)
+		{
+			return GCHandle.ToIntPtr(GCHandle.Alloc(obj, GCHandleType.Normal)) == Address;
+		}
+
+		public override int GetHashCode()
+		{
+			return Address.ToInt32();
+		}
+
+		public override string ToString()
+		{
+			return Address.ToString();
+		}
+
+		/// <summary>
+		/// Clone this memory object. this method will be allocate new memory area.
+		/// </summary>
+		/// <returns>New allocated memory object.</returns>
+		public object Clone()
+		{
+			return Allocation(this.Size);
 		}
 
 		public void Dispose()
 		{
-			Delete();
+			Marshal.FreeHGlobal(Address);
 		}
 	}
 }
--- UMemory/UMemory.cs
+++ UMemory/UMemory.cs
@@ -9,9 +9,19 @@ namespace UMemory
 {
     public static class UMemory
 	{
+		/// <summary>
+		/// Allocation Unmanaged memory area.
+		/// </summary>
+		/// <param name="size">Size of memory area.</param>
+		/// <returns>Allocated memory area. This object isn't garabge collectable.</returns>
 		public static Memory Allocation(int size)
 		{
 			return Memory.Allocation(size);
+		}
+
+		public static void Memcpy(Memory src, Memory dst, int length)
+		{
+			Array.Copy(src.Bytes, dst.Bytes, length);
 		}
 	}
 }
