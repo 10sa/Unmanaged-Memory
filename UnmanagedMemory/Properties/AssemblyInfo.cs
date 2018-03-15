6eac43fbf418c3d0cef42d626116b94919db5723
10sa <zenkins26@gmail.com>
2018-03-15 18:32:17 +0900

    Rename Solution, Project, assembly info.

--- UnmanagedMemory/Properties/AssemblyInfo.cs
+++ UnmanagedMemory/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("UMemory")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Unmanaged Memory")]
+[assembly: AssemblyDescription("https://github.com/10sa/Unmanaged-Memory")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UMemory")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Unmanaged Memory")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Tensa")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // ComVisible을 false로 설정하면 이 어셈블리의 형식이 COM 구성 요소에 
 // 표시되지 않습니다. COM에서 이 어셈블리의 형식에 액세스하려면
 // 해당 형식에 대해 ComVisible 특성을 true로 설정하세요.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
 [assembly: Guid("700687e0-5a4d-40b0-bee4-633381f662c6")]
